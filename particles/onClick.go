938b2f5e8cb15726960564fa0d02719e8ce93b51
Loan <loan.bouhier@gmail.com>
2023-01-04 12:13:28 +0100

    hbj

--- particles/onClick.go
+++ particles/onClick.go
@@ -11,12 +11,15 @@ func (s *System) onClick() {
 		for e := s.Content.Front(); e != nil; e = e.Next() {
 			p := e.Value.(*Particle)
 			// Si particule est à moins de 100px du pointeur
-			if int(math.Sqrt(float64((p.PositionY - float64(s.CursorY))*float64((p.PositionY - float64(s.CursorY))) + float64((p.PositionX - float64(s.CursorX)))*float64((p.PositionX - float64(s.CursorX)))))) < 100 {
+			if int(math.Sqrt(float64((p.PositionY - float64(s.CursorY))*float64((p.PositionY - float64(s.CursorY))) + float64((p.PositionX - float64(s.CursorX)))*float64((p.PositionX - float64(s.CursorX)))))) < 300 {
 				var vectorX float64 = float64(s.CursorX) - p.PositionX
 				var vectorY float64 = float64(s.CursorY) - p.PositionY
-				p.SpeedX = vectorX * 0.1
-				p.SpeedY = vectorY * 0.1	
+				p.SpeedX = vectorX * 0.05
+				p.SpeedY = vectorY * 0.05	
 			} 
+			if int(math.Sqrt(float64((p.PositionY - float64(s.CursorY))*float64((p.PositionY - float64(s.CursorY))) + float64((p.PositionX - float64(s.CursorX)))*float64((p.PositionX - float64(s.CursorX)))))) < 1 {
+				p.respawn()
+			}
 		}
 	}
 
