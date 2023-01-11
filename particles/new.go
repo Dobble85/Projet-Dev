923a31398c208523e3a9a50163b12bda4736dd5d
Loan <loan.bouhier@gmail.com>
2023-01-11 09:39:16 +0100

    ysépassékwa?

--- particles/new.go
+++ particles/new.go
@@ -3,6 +3,12 @@ package particles
 import (
 	"container/list"
 	"project-particles/config"
+	"image"
+	_ "image/jpeg"
+	_ "image/gif"
+	"net/http"
+	"github.com/hajimehoshi/ebiten/v2"
+	"strings"
 )
 
 // NewSystem est une fonction qui initialise un système de particules et le
@@ -14,9 +20,31 @@ func NewSystem() System {
 	var s System
 	s.Tick = 0
 	s.Content = list.New()
+
+	if config.General.Preset[:4] == "img-" {
+		url := config.General.Preset[4:]
+		response, _ := http.Get(url)
+		// Décoder l'image
+		img, _, _ := image.Decode(response.Body)
+		s.img = img
+		imgBounds := img.Bounds()
+		//strings.Contains(url, ".gif")
+		config.General.WindowSizeX = imgBounds.Max.X
+		config.General.WindowSizeY = imgBounds.Max.Y
+		ebiten.SetWindowSize(imgBounds.Max.X, imgBounds.Max.Y)
+
+		config.General.MaxParticles = (imgBounds.Max.X * imgBounds.Max.X) / 5
+		config.General.InitNumParticles = (imgBounds.Max.X * imgBounds.Max.X) / 1000
+		config.General.SpawnRate = float64((imgBounds.Max.X * imgBounds.Max.X) / 1000)
+	} else {
+		s.img = image.NewRGBA(image.Rect(0, 0, 10, 10))
+	}
+
 	s.ParticleSpawnX = config.General.SpawnX
 	s.ParticleSpawnY = config.General.SpawnY
 	s.ParticleColor = config.General.DefaultSpawnColor
+
+
 
 	for nbParticles := 0; nbParticles < config.General.InitNumParticles; nbParticles++ {
 		s.CreateParticle()
